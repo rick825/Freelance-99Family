872a34f03253d34caa75ba125bbe1c959a09919d
Kumar Das Akash <rickak825@gmail.com>
2024-01-06 20:23:04 +0530

    Updates - Refer to Readme

---
+++ assets/script/script.js
@@ -0,0 +1,38 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  console.log("running fixed");
+  var nav = document.querySelector('.nav');
+  var topnav = document.querySelector('.navtop');
+
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+       topnav.classList.add('navtophide');
+  } else {
+       topnav.classList.remove('navtophide');
+  }
+}
+
+function scrollToPopCat(){
+  console.log("Scrolling");
+  window.scrollTo({
+    top: 350,
+    behavior: "smooth" // You can change this to "auto" for instant scrolling
+  });
+}
+
+
+const navshow = () =>{
+  //console.log("nav show");
+  const ham = document.querySelector('.ham');
+  const navbot = document.querySelector('.navbot');
+
+  ham.addEventListener('click',()=>{
+    console.log('Shownav-->')
+   navbot.classList.toggle('navbotshow');
+  });
+}
+
+
+navshow();
